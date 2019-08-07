01deda4141e319bcb4b348abe795a1e12c06d8e8
github username <github email address>
2019-08-07 18:28:23 -0400

    Marisa and Aicha paired labwork

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,52 @@
+require 'pry'
+
 class Owner
-  # code goes here
+  #class methods:
+    #.all - return all instances of owner that have been created
+  @@all = []
+    #.count - return # of owners created
+  
+    #.reset_all resets owners
+  
+    attr_reader :name, :species, :say_species
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @say_species = "I am a human."
+    @@all << self
+  end
+
+  def self.all
+    return @@all
+  end
+
+  def self.count  
+    return self.all.count
+  end
+
+  def self.reset_all
+    @@all = []
+  end
+
+  def cats
+    Cat.all.select do |item|      
+      item.owner.name == self.name
+    end
+  end
+  
+  def dogs
+    Dog.all.select do |pup|
+      pup.owner == self
+    end
+  end
+
+
+
+  #know about pets
+
+  #buy pets
+
+
+
 end
