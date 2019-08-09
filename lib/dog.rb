993748c83acf3499315096e829c9c823a728e892
github username <github email address>
2019-08-09 12:26:04 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,32 @@
 class Dog
-  # code goes here
+
+  attr_accessor :owner, :mood
+  attr_reader :name
+  @@all = []
+
+  def initialize(name, owner)
+    @name, @owner, @mood = name, owner, "nervous"
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,93 @@
 class Owner
-  # code goes here
+
+
+  @@all = []
+  attr_reader :name, :species
+  
+  
+  
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+    
+  end
+
+  def say_species
+    return "I am a human."
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    self.all.length
+  end
+
+  def self.reset_all
+    self.all.clear
+  end
+
+  def cats
+    Cat.all.select do |kitty|
+      kitty.owner == self
+    end
+  end
+
+  def dogs
+    Dog.all.select do |pup|
+      pup.owner == self
+    end
+  end
+
+  def buy_cat(cat_name)
+    Cat.new(cat_name, self)
+  end
+
+  def buy_dog(dog_name)
+    Dog.new(dog_name, self)
+  end
+
+  def walk_dogs
+    my_dogs = self.dogs
+    my_dogs.each do |dog|
+      dog.mood = "happy"
+      
+    end
+  end
+
+    def feed_cats
+      self.cats.each do |cat|
+        cat.mood = "happy"
+      end
+    end
+
+    def sell_pets
+      my_pets = self.cats + self.dogs
+      my_pets.each do |pet|
+        pet.mood = "nervous"
+        pet.owner = nil
+      end
+    end
+
+    def list_pets
+      "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+    end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 end
